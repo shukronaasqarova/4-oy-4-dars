fe15dc32b32db51b1eaf7ed7e213279a8dea9a0f
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-05-24 00:38:23 +0500

    end

---
+++ index.js
@@ -0,0 +1,103 @@
+
+// 1 misol
+
+// alert("alisherni sayohatiga yordam qivoring");
+// let som = +prompt('alisher qancha puling bor;');
+// let samalyot = 500 ;
+// let mehmonhona = 250 ;
+// let aylanish = 120 ;
+// let jami ;
+// jami = samalyot*11000+mehmonhona*11000+sayohat*12354 ;
+// if ( a >= jami ){
+// /console.log('oq yol ALISHER');
+// }else{
+// console.log("ko'proq ishla ALISHER");
+// }
+
+
+
+// 2 misol
+
+// alert("tortburchak a va b tomonlarini kiriting ; bunda (1 <= a,b <= 10^9) ");
+// let a = +prompt('a tomonni kiriting;');
+// let b = +prompt('b tomonni kiriting;');
+// let yuza;
+// let perimatr;
+// yuza = a*b;
+// perimetr = (a+b)*2;
+// if(yuza > perimatr){
+// console.log(perimetr);
+// }else{
+// console.log(yuza);
+// }
+
+
+
+// 3 misol
+
+// alert("poyezd ketishi kerak bolgan vaqtni kiriting ")
+// let t = +prompt('jadvaldagi vaqt');
+// let T = +prompt('kechikkan vaqt');
+// let kechikkan_vaqt;
+// kechikkan_vaqt = t < 24 ? t : T+t % 24 ;
+
+
+
+// 4 misol
+
+// alert("haroratini kiriting");
+// let c = +prompt('c ni kiriting;');
+// let f=1.80+32;
+// let k=273;
+// let kelvin;
+// let far ;
+// kelvin = k+c;
+// far = f*c;
+// console.log(kelvin);
+// console.log(far);
+
+
+// 5 misol
+
+// alert ("tomoni N ga teng bolgan kopburchakning ichki burchaklar yegindisini toping");
+// let N = +prompt ("N");
+// let ichki_burchak = (180*(N-2));
+// console.log(ichki_burchak);
+
+
+
+// 6 misol
+
+// let hh = + prompt("0 dan 24 gacha");
+// let mm = + prompt("0 dan 60 gacha");
+// let t = + prompt("qancha vaqt otti");
+// let h = t/60;
+// h = (h);
+// let m = t%60
+// if ((h+hh)>=0 && (h+hh)<23){
+// h=h+hh;
+// }else{
+// h=h+hh-23;
+// }
+// if ((m+mm)>=0 && (m+mm)<60){
+// m=m+mm;
+// }else{
+// m=m+mm-60;
+// h= h + 1
+// }
+// console.log(h);
+// console.log (m);
+
+
+
+// 7 misol
+
+// alert("sardorning yoshi buvisinig yoshini yarmiga teng bolishini toping");
+// let buvi = +prompt ("buvasining yoshi");
+// let sardor = +prompt ("sardorning yoshi");
+// let yosh = (buvi-sardor);
+// let xammasi = (yosh/2);
+// console.log(xammasi);
+
+
+
